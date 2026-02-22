652f829c5aa9aaecf27fdd0955f744c0f5957695
Jasse <jassehansson2014@gmail.com>
2026-02-22 19:57:57 +0100

    Fix interface IChat and Chat

--- resources/model/interfaces/IChat.java
+++ resources/model/interfaces/IChat.java
@@ -1,9 +1,9 @@
 package resources.model.interfaces;
-
-import resources.model.Message;
+import java.util.Iterator;
 
 public interface IChat extends Iterable {
-    public void addMessage(Message msg);
-    public void removeMessage(Message msg);
-    public void editMessage(Message msg);
+    public void addMessage(IMessage msg);
+    public void removeMessage(IMessage msg);
+    public void editMessage(IMessage oldMsg, IMessage newMsg);
+    public Iterator<IMessage> iterator(int index);
 }
