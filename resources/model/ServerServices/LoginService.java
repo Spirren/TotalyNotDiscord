e6066919f503cefbc7a9fef6a129df6196de3438
Alfred <alfred04johnsson@gmail.com>
2026-02-24 11:12:00 +0100

    Add return user on successful login

--- resources/model/ServerServices/LoginService.java
+++ resources/model/ServerServices/LoginService.java
@@ -1,10 +1,13 @@
 package resources.model.ServerServices;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 import resources.model.LoginRequest;
-import resources.model.User;
 import resources.model.dispatcher.DispatchRequest;
+import resources.model.interfaces.IChat;
+import resources.model.interfaces.IUser;
 import resources.model.types.OperationType;
 import resources.sockets.ClientHandler;
 import server.database.PostgresConnectionProvider;
@@ -14,9 +17,13 @@ public class LoginService {
     public void login(LoginRequest lr, ClientHandler handler) {
         System.out.println("Login user " + lr.getUsername() + " with password " + lr.getPassword());
         try {
-            User user = SqlUtils.getUser(new PostgresConnectionProvider().getConnection(), lr.getUsername());
+            IUser user = SqlUtils.getUser(new PostgresConnectionProvider().getConnection(), lr.getUsername());
             if (user != null) {
                 handler.setUser(user);
+                ArrayList<IChat> chats = SqlUtils.getUserChats(new PostgresConnectionProvider().getConnection(), user.getID());
+                for (IChat chat : chats) {
+                    user.addChat(chat);
+                }
                 handler.send(new DispatchRequest(user, OperationType.LOGIN));
             } else {
                 handler.send(new DispatchRequest(lr, OperationType.ERROR));
