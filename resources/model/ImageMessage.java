922fea5e47a328c18868e665bd95ba49491b3b08
Alfred <alfred04johnsson@gmail.com>
2026-03-03 16:14:14 +0100

    Fix not serializable issue for BufferedImages

--- resources/model/ImageMessage.java
+++ resources/model/ImageMessage.java
@@ -1,7 +1,13 @@
 package resources.model;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
+
+import javax.imageio.ImageIO;
+
 import resources.model.interfaces.IImageMessage;
 import resources.model.interfaces.IMessageVisitor;
 import resources.model.interfaces.IUser;
@@ -9,27 +15,41 @@ import resources.model.interfaces.IUser;
 public class ImageMessage implements IImageMessage {
     private LocalDateTime timeSent;
     private LocalDateTime lastEdited;
-    private BufferedImage content;
+    private byte[] content;
     private IUser sender;
     private int index;
     private int chatID;
 
-    public ImageMessage(LocalDateTime timeSent, LocalDateTime lastEdited, BufferedImage content, IUser sender,
-            int index, int chatID) {
+    public ImageMessage(LocalDateTime timeSent, LocalDateTime lastEdited, BufferedImage content, IUser sender, int index, int chatID) {
+        if (content == null) throw new IllegalArgumentException();
         this.timeSent = timeSent;
         this.lastEdited = lastEdited;
-        this.content = content;
+        this.content = imageToBytes(content, "png");
         this.sender = sender;
         this.index = index;
         this.chatID = chatID;
     }
 
     public ImageMessage(LocalDateTime timeSent, BufferedImage content, IUser sender, int index, int chatID) {
+        if (content == null) throw new IllegalArgumentException();
         this.timeSent = timeSent;
-        this.content = content;
+        this.content = imageToBytes(content, "png");
         this.sender = sender;
         this.index = index;
         this.chatID = chatID;
+    }
+
+    private byte[] imageToBytes(BufferedImage image, String format) {
+        try {
+            if (image != null) {
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                ImageIO.write(image, format, bout);
+                return bout.toByteArray();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -49,7 +69,13 @@ public class ImageMessage implements IImageMessage {
 
     @Override
     public BufferedImage getContent() {
-        return content;
+        try {
+            ByteArrayInputStream bin = new ByteArrayInputStream(content);
+            return ImageIO.read(bin);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
